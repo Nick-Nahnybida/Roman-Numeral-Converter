3493a41d48e1c42208ef72304423478d48559151
Nick-Nahnybida <nicknahnybida@gmail.com>
2016-09-30 10:21:18 -0500

    Add files via upload

---
+++ Roman Numeral Converter/Roman Numeral Converter/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(40, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a decimal integer " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "in the range 1-10:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(61, 122)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 26)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Equivalent Roman " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Numeral:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'lblResult
+        '
+        Me.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblResult.Location = New System.Drawing.Point(205, 122)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(125, 23)
+        Me.lblResult.TabIndex = 2
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(205, 64)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(125, 20)
+        Me.txtInput.TabIndex = 3
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(119, 249)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 4
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(237, 249)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(430, 371)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Roman Numeral Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblResult As System.Windows.Forms.Label
+    Friend WithEvents txtInput As System.Windows.Forms.TextBox
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Roman Numeral Converter/Roman Numeral Converter/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim decInt As Integer
+
+        ' Convert variables
+        decInt = CInt(txtInput.Text)
+
+        ' Convert and display result
+        If decInt = 1 Then
+            lblResult.Text = "I"
+
+        ElseIf decInt = 2 Then
+            lblResult.Text = "II"
+
+        ElseIf decInt = 3 Then
+            lblResult.Text = "III"
+
+        ElseIf decInt = 4 Then
+            lblResult.Text = "IV"
+
+        ElseIf decInt = 5 Then
+            lblResult.Text = "V"
+
+        ElseIf decInt = 6 Then
+            lblResult.Text = "VI"
+
+        ElseIf decInt = 7 Then
+            lblResult.Text = "VII"
+
+        ElseIf decInt = 8 Then
+            lblResult.Text = "VIII"
+
+        ElseIf decInt = 9 Then
+            lblResult.Text = "IX"
+
+        ElseIf decInt = 10 Then
+            lblResult.Text = "X"
+        End If
+    End Sub
+End Class
